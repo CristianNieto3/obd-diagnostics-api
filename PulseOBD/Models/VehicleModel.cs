7f8b9b7fd43e7bd256c7d5bbf51b045eb4d819d9
CristianNieto3 <cristian.nieto20@icloud.com>
2026-03-27 00:27:10 -0500

    began working on some repository stuff

--- PulseOBD/Models/VehicleModel.cs
+++ PulseOBD/Models/VehicleModel.cs
@@ -4,8 +4,8 @@ public class Vehicle
 {
     public int Id {get; set;}
     public string VIN{get; set;}
-    public String Make{get; set;}
-    public String Model{get; set;}
+    public string Make{get; set;}
+    public string Model{get; set;}
     public int Year {get; set;}
     public DateTime CreatedAt{get;set;}
 
--- PulseOBD/Repositories/VehicleRepository.cs
+++ PulseOBD/Repositories/VehicleRepository.cs
@@ -1,23 +1,35 @@
 
 using PulseOBD.Models;
-
+using PulseOBD.Data;
+using Microsoft.EntityFrameworkCore;
 namespace PulseOBD.Repositories;
+
+//async can use await in its body. Use this in I/O bound operations mainly (network requests,file access, db queries etc..)
 
 public class EfVehicleRepository : IVehicleRepository
 {
+    private readonly ApplicationDbContext _context;
 
+    public EfVehicleRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public Task<List<Vehicle>> GetAll()
     {
-        throw new NotImplementedException();
+       return _context.Vehicles.AsNoTracking().ToListAsync();
     }
 
-     public Task<Vehicle> AddVehicle(Vehicle vehicle)
+     public async Task<Vehicle> AddVehicle(Vehicle vehicle)
     {
-        throw new NotImplementedException();
+        //add vehicle 
+        _context.Vehicles.Add(vehicle);
+        await _context.SaveChangesAsync();
+        return vehicle;
     }
 
     public Task<Vehicle?> GetById(int Id)
-    {
+    //if its not null, return it. otherwise throw an exception.
+   {
         throw new NotImplementedException();
     }
 
